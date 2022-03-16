8b2ce7e7f825b4bb2a45dd3210e1c183a87027e0
Jeffrey Nunez <jeffjnunez@gmail.com>
2022-03-16 11:56:21 -0700

    add sign-in and sign-up pages and functionality removed some unneeded boilerplate from create-react-app

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -20,6 +21,27 @@ const SignIn = () => {
         }));
     };
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth();
+
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+            if (userCredential.user) {
+                navigate('/');
+            }
+        } catch (error) {
+            console.log(error);
+            // TODO add toast component
+        }
+    };
+
     return (<>
         <div className='pageContainer'>
             <header>
@@ -29,7 +51,7 @@ const SignIn = () => {
             </header>
 
             <main>
-                <form>
+                <form onSubmit={onSubmit}>
                     <input
                         type='email'
                         className='emailInput'
